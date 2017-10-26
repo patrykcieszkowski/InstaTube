186da616b9d9a6688ec4bf92c3ad0ff3d8001cce
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-10-26 14:49:30 +0100

    Home/Home - seperated component to smaller chunks

--- src/components/Home/Home/index.js
+++ src/components/Home/Home/index.js
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 import UploadForm from '../../partials/UploadForm'
+import SignupInfo from './partials/SignupInfo'
 
 import style from './style'
 
@@ -17,40 +18,7 @@ export class Home extends React.Component {
           </Col>
         </Row>
         <Row className={css(style.grid.row, style.signup.box)}>
-          <Col xs="12">
-            <h1 className={css(style.signup.h1)}>Why should you register?</h1>
-          </Col>
-          <Col xs="12">
-            <ul className={css(style.signup.list)}>
-              <li className={css(style.signup.listItem)}>Manage uploads</li>
-              <li className={css(style.signup.listItem)}>PPV links</li>
-              <li className={css(style.signup.listItem)}>
-                Link availability 24h
-              </li>
-              <li className={css(style.signup.listItem)}>
-                More config options
-              </li>
-            </ul>
-          </Col>
-          <Col xs="12">
-            <Row>
-              <Col xs="10">
-                <Link to="/nav/auth" className={css(style.signup.signupButton)}>
-                  Sign me up for FREE!
-                </Link>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs="10">
-                <span className={css(style.signup.loginSpan)}>
-                  Already got an account?
-                  <Link to="/nav/auth" className={css(style.signup.loginLink)}>
-                    Login!
-                  </Link>
-                </span>
-              </Col>
-            </Row>
-          </Col>
+          <SignupInfo />
         </Row>
       </Container>
     )
---
+++ src/components/Home/Home/partials/SignupInfo.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { css } from 'aphrodite'
+import { Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import style from '../style'
+
+const SignupInfo = () => ([
+  <Col xs="12" key={0}>
+    <h1 className={css(style.signup.h1)}>Why should you register?</h1>
+  </Col>,
+  <Col xs="12" key={1}>
+    <ul className={css(style.signup.list)}>
+      <li className={css(style.signup.listItem)}>Manage uploads</li>
+      <li className={css(style.signup.listItem)}>PPV links</li>
+      <li className={css(style.signup.listItem)}>
+        Link availability 24h
+      </li>
+      <li className={css(style.signup.listItem)}>
+        More config options
+      </li>
+    </ul>
+  </Col>,
+  <Col xs="12" key={2}>
+    <Row>
+      <Col xs="10">
+        <Link to="/nav/auth" className={css(style.signup.signupButton)}>
+          Sign me up for FREE!
+        </Link>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs="10">
+        <span className={css(style.signup.loginSpan)}>
+          Already got an account?
+          <Link to="/nav/auth" className={css(style.signup.loginLink)}>
+            Login!
+          </Link>
+        </span>
+      </Col>
+    </Row>
+  </Col>
+])
+
+export default SignupInfo
