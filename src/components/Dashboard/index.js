491201f3d0cf9dfe1191f105b2edf5a77f6148b7
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-10-28 14:54:23 +0100

    components/Dasboard - exported nav components

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,11 @@
 import Main from './Main'
+import Profile from './Profile'
+import Payout from './Payout'
+import Premium from './Premium'
 
 export default {
-  Main
+  Main,
+  Profile,
+  Payout,
+  Premium
 }
