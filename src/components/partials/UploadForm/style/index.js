fba4ee7f8d37b28f18863f983466fb469f838f74
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-01 20:34:48 +0000

    Revert "components/UploadForm - layout improvements"
    This reverts commit 9641c80fb688c30646faacec0b27327bb7f8adb2.

--- src/components/partials/UploadForm/style/index.js
+++ src/components/partials/UploadForm/style/index.js
@@ -5,15 +5,11 @@ import form from './form'
 import message from './message'
 import views from './views'
 import links from './links'
-import sliders from './sliders'
-import errors from './errors'
 
 export default {
   form: StyleSheet.create(form),
   message: StyleSheet.create(message),
   views: StyleSheet.create(views),
   dropzone: StyleSheet.create(dropzone),
-  links: StyleSheet.create(links),
-  sliders: StyleSheet.create(sliders),
-  errors: StyleSheet.create(errors)
+  links: StyleSheet.create(links)
 }
