55ca1714fa5ec928567519aae0ab303b0812dd7e
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 18:43:52 +0000

    components/UploadForm - added breakpoint for static and dynamic width

--- src/components/partials/UploadForm/style/form.js
+++ src/components/partials/UploadForm/style/form.js
@@ -4,7 +4,10 @@ export const formStyle = {
     borderRadius: `5px`,
     padding: `8px`,
     boxShadow: `0px 8px 17px 0px rgba(0,0,0,0.15)`,
-    width: `420px`,
+    width: `100%`,
+    '@media (min-width: 992px)': {
+      width: `420px`,
+    },
     '@media (min-width: 1600px)': {
       width: `470px`
     }
