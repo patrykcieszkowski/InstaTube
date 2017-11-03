49a3c6acc6f36ad1ae8f4a3aa02cd20f89f6149c
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 10:06:08 +0000

    containers/Home - applied targeted background to the container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -24,7 +24,7 @@ export class HomeContainer extends React.Component {
     ]
 
     return (
-      <Components.partials.Container fluid noPadding fullHeight>
+      <Components.partials.Container fluid noPadding minHeight bg={`home`}>
         <Components.partials.Navbar />
         <Components.partials.Container fluid noPadding fullHeight>
           <Components.Home.Home />
