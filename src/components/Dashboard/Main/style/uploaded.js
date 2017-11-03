a3ce609bd11b60fa6ff59685d89a4d8bb5090387
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 10:14:10 +0000

    components/Dashboard - uploaded partial styling (desktop devices)

--- src/components/Dashboard/Main/style/uploaded.js
+++ src/components/Dashboard/Main/style/uploaded.js
@@ -4,10 +4,25 @@ export const uploadedStyle = {
     paddingTop: `0px`
   },
   box: {
-    background: `white`,
     borderRadius: `5px`,
     margin: `3px 0`,
-    padding: `5px`
+    padding: `5px`,
+    '@media (max-width: 1199px)': {
+      background: `white`,
+    },
+    '@media (min-width: 1200px)': {
+      height: `50px`,
+      overflow: `hidden`
+    }
+  },
+  boxActive: {
+    background: `white`
+  },
+  titleSpan: {
+    display: `block`,
+    fontSize: `12px`,
+    fontWeight: `100`,
+    color: `#8c9194`
   },
   labelSpan: {
     display: `block`,
@@ -18,11 +33,67 @@ export const uploadedStyle = {
     fontWeight: `800`,
     color: `#3f295a`
   },
+  valueBox: {
+    padding: `0 5px`,
+    height: `26px`,
+    '@media (min-width: 1200px)': {
+      height: `40px`
+    }
+  },
   valueSpan: {
-    display: `block`,
+    display: `flex`,
+    alignItems: `center`,
     padding: `1px`,
-    fontSize: `16px`,
-    color: `#3f295a`
+    fontSize: `14px`,
+    color: `#3f295a`,
+    '@media (min-width: 1200px)': {
+      justifyContent: `center`,
+      height: `40px`
+    }
+  },
+  copyBlock: {
+    position: `absolute`,
+    top: 0,
+    right: `-300px`,
+    height: `40px`,
+    background: `#edf2f6`,
+    borderRadius: `5px`,
+    padding: 0,
+    opacity: 0,
+    transition: `0.2s`
+  },
+  copyBlockActive: {
+    right: 0,
+    opacity: 1
+  },
+  copyBlockWrapper: {
+    flex: 1,
+    margin: 0,
+    padding: `0 5px`
+  },
+  copyInputWrapper: {
+    padding: 0
+  },
+  copyInput: {
+    background: `none`,
+    border: `none`,
+    fontSize: `14px`,
+    // width: `100%`,
+    color: `#3f295a`,
+    fontWeight: `600`
+  },
+  actionIconBox: {
+    padding: 0
+  },
+  actionDateToggle: {
+    position: `absolute`,
+    top: `0`,
+    right: `0`,
+    left: `0`,
+    transition: `0.2s`
+  },
+  actionDateToggleActive: {
+    top: `-40px`
   },
   valuePrice: {
     fontWeight: `100`
@@ -49,6 +120,7 @@ export const uploadedStyle = {
     background: `#7eca12`
   },
   actionIcon: {
+    textDecoration: `none`,
     fontSize: `20px`,
     padding: `2px 4px`,
     paddingLeft: `2px`,
