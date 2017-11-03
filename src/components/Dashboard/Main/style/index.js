43c5348e8acb8ff96a2f75950a5ecfb94a4792f6
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 11:35:02 +0000

    component/Dashboard/Main - style - exported main.js

--- src/components/Dashboard/Main/style/index.js
+++ src/components/Dashboard/Main/style/index.js
@@ -4,10 +4,12 @@ import header from './header'
 import payments from './payments'
 import uploaded from './uploaded'
 import shared from './shared'
+import main from './main'
 
 export default {
   shared: StyleSheet.create(shared),
   header: StyleSheet.create(header),
   payments: StyleSheet.create(payments),
-  uploaded: StyleSheet.create(uploaded)
+  uploaded: StyleSheet.create(uploaded),
+  main: StyleSheet.create(main)
 }
