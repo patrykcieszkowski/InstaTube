ea1156d7ce64358cae0a165f45202125d9db0545
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 11:35:25 +0000

    components/partials - exported ScrollArea component

--- src/components/partials/index.js
+++ src/components/partials/index.js
@@ -3,11 +3,13 @@ import SidebarContainer from './SidebarContainer'
 import SidebarContent from './SidebarContent'
 import Container from './Container'
 import PieTimer from './PieTimer'
+import ScrollArea from './ScrollArea'
 
 export default {
   Navbar,
   SidebarContainer,
   SidebarContent,
   Container,
-  PieTimer
+  PieTimer,
+  ScrollArea
 }
