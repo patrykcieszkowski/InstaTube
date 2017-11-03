b35f70f31de10f416dedb46f5cc8b34213e90338
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 11:32:14 +0000

    utils/secondsToTime - added days counter

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,16 @@
 const secondsToTime = secs => {
   const sec_num = parseInt(secs, 10)
-  const hours = Math.floor(sec_num / 3600) % 24
+  const days = Math.floor(sec_num / (60 * 60 * 24))
+  const hours = Math.floor(sec_num / (60 * 60)) % 24
   const minutes = Math.floor(sec_num / 60) % 60
   const seconds = sec_num % 60
 
-  return [[hours, ['h']], [minutes, ['m']], [seconds, ['s', 'sec']]]
+  return [
+    [days, ['d']],
+    [hours, ['h']],
+    [minutes, ['m']],
+    [seconds, ['s', 'sec']]
+  ]
     .map(v => (v[0] < 10 ? ['0' + v[0], v[1]] : v))
     .filter((v, i) => v[0] !== '00' || i > 1)
 }
