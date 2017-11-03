e4978edc016984cffc786cb3699fda03933a4669
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 18:46:33 +0000

    components/Home - style/grid - added top/bottom margin (navbar)

--- src/components/Home/Home/style/grid.js
+++ src/components/Home/Home/style/grid.js
@@ -2,7 +2,7 @@ export const gridStyle = {
   mainRow: {
     '@media only screen and (min-width : 992px)': {
       flexBasis: `100%`,
-      margin: 0
+      margin: `85px 0`
     }
   },
   row: {
