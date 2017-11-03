224b1f4f98df9672ef3c7af4a5a93a1893a5d229
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 10:11:53 +0000

    components/Dashboard - added header layout (desktop support)

--- src/components/Dashboard/Main/partials/Header.js
+++ src/components/Dashboard/Main/partials/Header.js
@@ -8,10 +8,33 @@ import style from '../style'
 export const DashboardHeader = () => (
   <Col xs="12">
     <Row>
-      <Col xs="12">
+      <Col className={`d-none d-xl-flex`}>
+        <span className={css(style.header.welcomeMessage)}>
+          We are happy to see you again Piotr Szymański
+        </span>
+      </Col>
+      <Col
+        className={`d-none d-xl-flex justify-content-center align-items-center`}
+      >
+        <span className={css(style.header.withdrawMessage)}>
+          You can easily withdraw your money with secure payments
+        </span>
+      </Col>
+
+      <Col className={`d-none d-xl-flex`} xs="3" />
+
+      <Col
+        xs="12"
+        xl=""
+        className={`d-flex justify-content-center align-items-center`}
+      >
         <span className={css(style.header.amountSpan)}>$1231,00</span>
       </Col>
-      <Col xs="12">
+      <Col
+        xs="12"
+        xl=""
+        className={`d-flex justify-content-center align-items-center`}
+      >
         <Link to="#" className={css(style.header.button)}>
           Withdraw
         </Link>
