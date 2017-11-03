cc99084a1af32e36243bc53ec1cd0a87f16e55ad
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 10:07:49 +0000

    components/UploadForm - set predefined width size

--- src/components/partials/UploadForm/style/form.js
+++ src/components/partials/UploadForm/style/form.js
@@ -3,7 +3,11 @@ export const formStyle = {
     background: `white`,
     borderRadius: `5px`,
     padding: `8px`,
-    boxShadow: `0px 8px 17px 0px rgba(0,0,0,0.15)`
+    boxShadow: `0px 8px 17px 0px rgba(0,0,0,0.15)`,
+    width: `420px`,
+    '@media (min-width: 1600px)': {
+      width: `470px`
+    }
   },
   group: {
     marginBottom: `10px`
