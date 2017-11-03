59c5549a597c866af61fa7b2bb901e732828fd0b
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 11:31:29 +0000

    component/Dashboard - layout update (desktop)

--- src/components/Dashboard/Main/index.js
+++ src/components/Dashboard/Main/index.js
@@ -3,14 +3,17 @@ import { css } from 'aphrodite'
 import { Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
-import { Scrollbars } from 'react-custom-scrollbars'
-
+import ScrollArea from '../../partials/ScrollArea'
+import UploadForm from '../../partials/UploadForm'
 import Container from '../../partials/Container'
+
 import Header from './partials/Header'
 import PaymentsList from './partials/PaymentsList'
-import UploadedList from './partials/UploadedList'
-
+import Uploaded from './partials/Uploaded'
+import UploadedListTitles from './partials/Uploaded/partials/titlesRow'
 import style from './style'
+
+const uploadFilesList = require('../../../json/dashboard-upload-files.json')
 
 export class Main extends React.Component {
   render() {
@@ -20,31 +23,48 @@ export class Main extends React.Component {
           <Header />
         </Row>
         <Row>
-          <Col xs="12">
+          <Col xs="12" className={`d-xl-none`}>
             <Link to="#" className={css(style.header.button)}>
               Add new file
             </Link>
           </Col>
-          <Col xs="12" sm="6" lg="6" className={css(style.uploaded.wrapper)}>
+          <Col
+            xl="auto"
+            className={`d-none d-xl-block ${css(style.main.colWrapper)}`}
+          >
+            <UploadForm />
+          </Col>
+          <Col
+            xs="12"
+            xl=""
+            className={css(style.uploaded.wrapper, style.main.colWrapper)}
+          >
             <Row>
               <Col xs="12">
                 <h3 className={css(style.shared.h3)}>Uploaded files</h3>
               </Col>
+              <Col xs="12" className={`d-none d-xl-block`}>
+                <UploadedListTitles />
+              </Col>
               <Col xs="12">
-                <Scrollbars style={{ width: '100%', height: `200px` }}>
-                  <UploadedList />
-                </Scrollbars>
+                <ScrollArea style={{ width: '100%', height: `382px` }}>
+                  <Uploaded items={uploadFilesList} />
+                </ScrollArea>
               </Col>
             </Row>
           </Col>
-          <Col xs="12" sm="6" lg="3" className={css(style.payments.wrapper)}>
+          <Col
+            xs="12"
+            xl="3"
+            className={css(style.payments.wrapper, style.main.colWrapper)}
+          >
             <Col xs="12">
               <h3 className={css(style.shared.h3)}>Payment history</h3>
             </Col>
             <Col xs="12" className={css(style.payments.content)}>
-              <Scrollbars style={{ width: '100%', height: `200px` }}>
+              <ScrollArea style={{ width: '100%', height: `200px` }}>
                 <PaymentsList />
-              </Scrollbars>
+              </ScrollArea>
             </Col>
           </Col>
         </Row>
