b9ab8150c0577c3e0ecd099a093284b61ea41613
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 10:11:22 +0000

    components/Dashboard - added header styling for (desktop support)

--- src/components/Dashboard/Main/style/header.js
+++ src/components/Dashboard/Main/style/header.js
@@ -1,8 +1,12 @@
 export const headerStyle = {
   wrapper: {
+    marginTop: `85px`,
     paddingBottom: `20px`,
     marginBottom: `20px`,
-    borderBottom: `1px solid #e3e9f0`
+    borderBottom: `1px solid #e3e9f0`,
+    '@media (min-width: 1200px)': {
+      padding: `20px`
+    }
   },
   amountSpan: {
     display: `block`,
@@ -13,6 +17,7 @@ export const headerStyle = {
   },
   button: {
     display: `block`,
+    width: `100%`,
     color: `white`,
     textAlign: `center`,
     background: `#e1306c`,
@@ -21,6 +26,16 @@ export const headerStyle = {
     padding: `8px 0`,
     fontSize: `19px`,
     fontWeight: `600`
+  },
+  welcomeMessage: {
+    color: `#3f295a`,
+    display: `block`,
+    fontSize: `24px`,
+    fontWeight: `600`
+  },
+  withdrawMessage: {
+    fontSize: `14px`,
+    color: `#737679`
   }
 }
 
