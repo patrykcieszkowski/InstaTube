b1567d12b89481b09c5f9cdde4e6293b32970c3f
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-03 14:26:48 +0000

    components/Dashboard/Main - sharedStyle - h3 - added desktop styling

--- src/components/Dashboard/Main/style/shared.js
+++ src/components/Dashboard/Main/style/shared.js
@@ -4,7 +4,13 @@ const sharedStyle = {
     fontWeight: `100`,
     textAlign: `center`,
     margin: `20px 0`,
-    color: `#c4c9cd`
+    color: `#c4c9cd`,
+    '@media (min-width: 1200px)': {
+      margin: `0 0 30px 0`,
+      textAlign: `left`,
+      fontSize: `20px`,
+      color: `#9ca2a5`
+    }
   }
 }
 
