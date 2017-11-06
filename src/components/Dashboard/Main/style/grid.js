f31b4fcaacd8ffddd7568cba140f45c6d4e49bdf
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-06 15:01:59 +0000

    created grid.js (styling)

---
+++ src/components/Dashboard/Main/style/grid.js
@@ -0,0 +1,19 @@
+export const gridStyle = {
+  'd-xxxxl-none': {
+    '@media (min-width: 1800px)': {
+      display: `none`
+    }
+  },
+  'd-xxxxl-flex': {
+    '@media (min-width: 1800px)': {
+      display: `flex`
+    }
+  },
+  'd-xxxxl-block': {
+    '@media (min-width: 1800px)': {
+      display: `block`
+    }
+  }
+}
+
+export default gridStyle
--- src/components/Dashboard/Main/style/index.js
+++ src/components/Dashboard/Main/style/index.js
@@ -5,11 +5,13 @@ import payments from './payments'
 import uploaded from './uploaded'
 import shared from './shared'
 import main from './main'
+import grid from './grid'
 
 export default {
   shared: StyleSheet.create(shared),
   header: StyleSheet.create(header),
   payments: StyleSheet.create(payments),
   uploaded: StyleSheet.create(uploaded),
-  main: StyleSheet.create(main)
+  main: StyleSheet.create(main),
+  grid: StyleSheet.create(grid)
 }
