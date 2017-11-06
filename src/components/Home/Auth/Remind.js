2792d9f97052d6d3702b5fbf5ea278d894197eff
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-06 00:21:33 +0000

    - adds SidebarHeader - replaces reactstrap Col with custom Column component

--- src/components/Home/Auth/Remind.js
+++ src/components/Home/Auth/Remind.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { css } from 'aphrodite'
+import { Container, Row, Col } from 'reactstrap'
 
+import style from './style'
+import Column from '../../partials/Col'
+import SidebarHeader from '../../partials/SidebarHeader'
+import SignupInfo from '../partials/SignupInfo'
+import ScrollArea from '../../partials/ScrollArea'
 import RemindForm from './partials/RemindForm'
 
 export class Auth extends React.Component {
   render() {
     return (
       <Container fluid>
-        <RemindForm />
+        <Row className={css(style.main.wrapper)}>
+          <Column
+            xl="auto"
+            xxxxl="3"
+            className={`d-none d-xl-flex align-items-start ${css(
+              style.sider.wrapper
+            )}`}
+          >
+            <Row className={css(style.sider.box)} />
+          </Column>
+          <Col xs="12" xl="12" className={css(style.content.wrapper)}>
+            <Row>
+              <SidebarHeader title={`Remind Password`} md={false} paddingTop />
+            </Row>
+            <RemindForm />
+          </Col>
+        </Row>
       </Container>
     )
   }
