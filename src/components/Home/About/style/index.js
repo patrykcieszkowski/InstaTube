ef6c99e69e99388e2b82174e7cea740656a3126d
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-06 00:37:41 +0000

    - moves main.js content into content.js (styling) - replaces reactstrap Col component with Column - adds SidebarHeader

--- src/components/Home/About/style/index.js
+++ src/components/Home/About/style/index.js
@@ -2,8 +2,10 @@ import { StyleSheet } from 'aphrodite'
 
 import main from './main'
 import sider from './sider'
+import content from './content'
 
 export default {
   main: StyleSheet.create(main),
-  sider: StyleSheet.create(sider)
+  sider: StyleSheet.create(sider),
+  content: StyleSheet.create(content)
 }
--- src/components/Home/About/style/main.js
+++ src/components/Home/About/style/main.js
@@ -1,19 +1,7 @@
 const mainStyle = {
   wrapper: {
-    height: `calc(100% - 20px)`
-  },
-  h3: {
-    color: `#3f295a`,
-    fontSize: `18px`,
-    fontWeight: `600`
-  },
-  header: {
-    paddingTop: `10px`,
-    paddingBottom: `10px`
-  },
-  p: {
-    color: `#1c1c1d`,
-    fontSize: `14px`
+    height: `100%`,
+    padding: `0 45px`
   }
 }
 
