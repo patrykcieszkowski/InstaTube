fde45b1b2a89d71085129d38978b187953156d6a
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-06 19:28:08 +0000

    moved form into partials dir

--- src/components/partials/UploadForm/index.js
+++ src/components/partials/UploadForm/index.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { css } from 'aphrodite'
 import { Form, FormGroup, Input, Button, Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom'
-import Dropzone from 'react-dropzone'
 import attrAccept from 'attr-accept'
 
 import style from './style'
 
+import UploadForm from './partials/UploadForm'
+
 const ATTR_ACCEPT_ALL = 'video/*,audio/*,image/*'
 const ATTR_ACCEPT_MEDIA = 'video/*,audio/*'
 
-export class UploadForm extends React.Component {
+export class Upload extends React.Component {
   constructor() {
     super()
     this.state = {
+      filename: '',
+      message: '',
       upload: {
         file: null,
         error: false
@@ -53,6 +56,20 @@ export class UploadForm extends React.Component {
         file: files[0],
         error: false
       }
+    })
+  }
+
+  onFilenameChange(e) {
+    this.setState({
+      ...this.state,
+      filename: e.target.value
+    })
+  }
+
+  onMessageChange(e) {
+    this.setState({
+      ...this.state,
+      message: e.target.value
     })
   }
 
@@ -121,243 +138,25 @@ export class UploadForm extends React.Component {
 
   render() {
     return (
-      <Form
-        className={css(style.form.form)}
-        onSubmit={this.onFormSubmit.bind(this)}
-      >
-        <FormGroup className={css(style.form.group)}>
-          <Dropzone
-            maxSize={20000000}
-            accept={ATTR_ACCEPT_ALL}
-            multiple={false}
-            onDropRejected={this.onDropRejected.bind(this)}
-            onDropAccepted={this.onDropAccepted.bind(this)}
-            className={`${css(
-              style.dropzone.wrapper
-            )} d-flex align-items-center justify-content-center`}
-          >
-            <Row className={css(style.form.row)}>
-              <Col className="justify-content-center">
-                <button type="button" className={css(style.dropzone.button)}>
-                  <i
-                    className={`la la-plus ${css(style.dropzone.buttonIcon)}`}
-                    aria-hidden="true"
-                    title="add"
-                  />
-                </button>
-              </Col>
-              <Col className="d-flex flex-column justify-content-center">
-                <Row className={css(style.form.row)}>
-                  <span className={css(style.dropzone.titleSpan)}>
-                    Add your file
-                  </span>
-                </Row>
-                <Row className={css(style.form.row)}>
-                  <span className={css(style.dropzone.descSpan)}>
-                    Maximum{' '}
-                    <span className={css(style.dropzone.boldSpan)}>
-                      2.00GB / 12h
-                    </span>
-                  </span>
-                </Row>
-              </Col>
-            </Row>
-          </Dropzone>
-        </FormGroup>
-        <FormGroup
-          className={css(style.form.group)}
-          style={{ display: !this.state.upload.error ? `none` : `block` }}
-        >
-          <Row className={css(style.errors.fileErrorWrapper)}>
-            <Col className={css(style.errors.fileErrorBox)} xs="10">
-              <span className={css(style.errors.fileErrorSpan)}>
-                {this.state.upload.error}
-              </span>
-            </Col>
-            <Col>
-              <span
-                className={css(style.errors.fileErrorCloseButton)}
-                onClick={() =>
-                  this.setState({ ...this.state, upload: { error: null } })}
-              >
-                <i
-                  className={`la la-close ${css(
-                    style.errors.fileErrorCloseIcon
-                  )}`}
-                  aria-hidden="true"
-                  title="close"
-                />
-              </span>
-            </Col>
-          </Row>
-        </FormGroup>
-        <FormGroup className={css(style.form.group, style.message.box)}>
-          <Input placeholder="Message" className={css(style.message.input)} />
-        </FormGroup>
-        <FormGroup className={css(style.form.group, style.links.box)}>
-          <Row className={css(style.form.row)}>
-            <Col
-              xs="7"
-              className={css(
-                style.links.wrapper,
-                style.links.instagramLinkWrapper
-              )}
-            >
-              <Link
-                to="/nav/auth"
-                onClick={this.onLinkClick.bind(this, 'instagram')}
-                className={css(
-                  style.links.link,
-                  this.state.links.instagram ? style.links.active : ''
-                )}
-              >
-                Instagram follow for view
-              </Link>
-            </Col>
-            <Col
-              className={css(style.links.wrapper, style.links.PPVLinkWrapper)}
-            >
-              <Link
-                to="/nav/auth"
-                onClick={this.onLinkClick.bind(this, 'ppv')}
-                className={css(
-                  style.links.link,
-                  this.state.links.ppv ? style.links.active : ''
-                )}
-              >
-                Pay per view
-              </Link>
-            </Col>
-          </Row>
-        </FormGroup>
-        <FormGroup className={css(style.form.group)}
-          style={{
-            display:
-              (this.state.upload.file &&
-                attrAccept(this.state.upload.file, ATTR_ACCEPT_MEDIA)) ||
-              Object.keys(this.state.links).filter(a => this.state.links[a])
-                .length
-                ? `none`
-                : `block`
-          }}
-        >
-          <Row className={css(style.form.row)}>
-            <Col xs="12">
-              <h4 className={css(style.sliders.h4)}>Display time</h4>
-            </Col>
-          </Row>
-          <Row className={css(style.form.row)}>
-            <Col xs="10" className={css(style.sliders.wrapper)}>
-              <input
-                type="range"
-                min="1"
-                max="100"
-                value={this.state.displayTime}
-                onChange={this.onDisplayTimeChange.bind(this)}
-                className={css(style.sliders.slider)}
-              />
-            </Col>
-            <Col xs="2" className={css(style.sliders.labelWrapper)}>
-              <span className={css(style.sliders.label)}>
-                {this.state.displayTime}h
-              </span>
-            </Col>
-          </Row>
-        </FormGroup>
-
-        <FormGroup
-          className={css(style.form.group)}
-        >
-          <Row className={css(style.form.row)}>
-            <Col xs="12">
-              <h4 className={css(style.sliders.h4)}>Validity period</h4>
-            </Col>
-          </Row>
-          <Row className={css(style.form.row)}>
-            <Col xs="5" className={css(style.sliders.wrapper)}>
-              <input
-                type="range"
-                min="1"
-                max="100"
-                value={this.state.validity.hour}
-                onChange={this.onValidityChange.bind(this, 'hour')}
-                className={css(style.sliders.slider)}
-              />
-            </Col>
-            <Col xs="1" className={css(style.sliders.labelWrapper)}>
-              <span className={css(style.sliders.label)}>
-                {this.state.validity.hour}h
-              </span>
-            </Col>
-            <Col xs="4" className={css(style.sliders.wrapper)}>
-              <input
-                type="range"
-                min="0"
-                max="4"
-                value={this.state.validity.minute / 15}
-                onChange={this.onValidityChange.bind(this, 'minute')}
-                className={css(style.sliders.slider)}
-              />
-            </Col>
-            <Col xs="2" className={css(style.sliders.labelWrapper)}>
-              <span className={css(style.sliders.label)}>
-                {this.state.validity.minute}m
-              </span>
-            </Col>
-          </Row>
-        </FormGroup>
-
-        <FormGroup className={css(style.form.group, style.views.box)}>
-          <Row className={css(style.form.row)}>
-            <Col xs="12">
-              <h4 className={css(style.sliders.h4)}>Number of views</h4>
-            </Col>
-          </Row>
-          <Row className={css(style.form.row)}>
-            <Col xs="7" className={css(style.views.inputWrapper)}>
-              <Input
-                onChange={this.onViewsNumberChange.bind(this)}
-                type="text"
-                value={this.state.views.view ? this.state.views.number : ''}
-                placeholder="Number of views"
-                className={css(
-                  style.views.input,
-                  !this.state.views.view ? style.views.active : ''
-                )}
-                disabled={!this.state.views.view}
-              />
-            </Col>
-            <Col className="d-flex flex-column justify-content-center">
-              <input
-                type="checkbox"
-                id="views_checkbox"
-                checked={this.state.views.view}
-                onChange={this.toggleCheckbox.bind(this)}
-                className={css(style.views.checkbox)}
-              />
-              <label
-                htmlFor="views_checkbox"
-                className={css(style.views.label)}
-              >
-                <span
-                  className={css(
-                    this.state.views.view
-                      ? style.views.spanChecked
-                      : style.views.span
-                  )}
-                >
-                  No limit views
-                </span>
-              </label>
-            </Col>
-          </Row>
-        </FormGroup>
-        <Button type="submit" className={css(style.form.submitButton)}>
-          Create link!
-        </Button>
-      </Form>
+      <UploadForm
+        onFormSubmit={this.onFormSubmit.bind(this)}
+        onDropRejected={this.onDropRejected.bind(this)}
+        onDropAccepted={this.onDropAccepted.bind(this)}
+        onFilenameChange={this.onFilenameChange.bind(this)}
+        onMessageChange={this.onMessageChange.bind(this)}
+        onInstagramClick={this.onLinkClick.bind(this, 'instagram')}
+        onPPVClick={this.onLinkClick.bind(this, 'ppv')}
+        onDisplayTimeChange={this.onDisplayTimeChange.bind(this)}
+        onValidityMinuteChange={this.onValidityChange.bind(this, 'minute')}
+        onValidityHourChange={this.onValidityChange.bind(this, 'hour')}
+        onViewsNumberChange={this.onViewsNumberChange.bind(this)}
+        onCheckboxClick={this.toggleCheckbox.bind(this)}
+        onErrorCloseClick={() =>
+          this.setState({ ...this.state, upload: { error: null } })}
+        state={this.state}
+      />
     )
   }
 }
 
-export default UploadForm
+export default Upload
---
+++ src/components/partials/UploadForm/partials/UploadForm.js
@@ -0,0 +1,241 @@
+import React from 'react'
+import { css } from 'aphrodite'
+import { Form, FormGroup, Input, Button, Col, Row } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import Dropzone from 'react-dropzone'
+import attrAccept from 'attr-accept'
+
+import style from '../style'
+
+const ATTR_ACCEPT_ALL = 'video/*,audio/*,image/*'
+const ATTR_ACCEPT_MEDIA = 'video/*,audio/*'
+
+export const UploadForm = props => (
+  <Form className={css(style.form.form)} onSubmit={props.onFormSubmit}>
+    <FormGroup className={css(style.form.group)}>
+      <Dropzone
+        maxSize={20000000}
+        accept={ATTR_ACCEPT_ALL}
+        multiple={false}
+        onDropRejected={props.onDropRejected}
+        onDropAccepted={props.onDropAccepted}
+        className={`${css(
+          style.dropzone.wrapper
+        )} d-flex align-items-center justify-content-center`}
+      >
+        <Row className={css(style.form.row)}>
+          <Col className="justify-content-center">
+            <button type="button" className={css(style.dropzone.button)}>
+              <i
+                className={`la la-plus ${css(style.dropzone.buttonIcon)}`}
+                aria-hidden="true"
+                title="add"
+              />
+            </button>
+          </Col>
+          <Col className="d-flex flex-column justify-content-center">
+            <Row className={css(style.form.row)}>
+              <span className={css(style.dropzone.titleSpan)}>
+                Add your file
+              </span>
+            </Row>
+            <Row className={css(style.form.row)}>
+              <span className={css(style.dropzone.descSpan)}>
+                Maximum{' '}
+                <span className={css(style.dropzone.boldSpan)}>
+                  2.00GB / 12h
+                </span>
+              </span>
+            </Row>
+          </Col>
+        </Row>
+      </Dropzone>
+    </FormGroup>
+    <FormGroup
+      className={css(style.form.group)}
+      style={{ display: !props.state.upload.error ? `none` : `block` }}
+    >
+      <Row className={css(style.errors.fileErrorWrapper)}>
+        <Col className={css(style.errors.fileErrorBox)} xs="10">
+          <span className={css(style.errors.fileErrorSpan)}>
+            {props.state.upload.error}
+          </span>
+        </Col>
+        <Col>
+          <span
+            className={css(style.errors.fileErrorCloseButton)}
+            onClick={props.onErrorCloseClick}
+          >
+            <i
+              className={`la la-close ${css(style.errors.fileErrorCloseIcon)}`}
+              aria-hidden="true"
+              title="close"
+            />
+          </span>
+        </Col>
+      </Row>
+    </FormGroup>
+    <FormGroup className={css(style.form.group, style.message.box)}>
+      <Input
+        placeholder="Message"
+        className={css(style.message.input)}
+        value={props.state.message}
+        onChange={props.onMessageChange}
+      />
+    </FormGroup>
+    <FormGroup className={css(style.form.group, style.links.box)}>
+      <Row className={css(style.form.row)}>
+        <Col
+          xs="7"
+          className={css(style.links.wrapper, style.links.instagramLinkWrapper)}
+        >
+          <Link
+            to="/nav/auth"
+            onClick={props.onInstagramClick}
+            className={css(
+              style.links.link,
+              props.state.links.instagram ? style.links.active : ''
+            )}
+          >
+            Instagram follow for view
+          </Link>
+        </Col>
+        <Col className={css(style.links.wrapper, style.links.PPVLinkWrapper)}>
+          <Link
+            to="/nav/auth"
+            onClick={props.onPPVClick}
+            className={css(
+              style.links.link,
+              props.state.links.ppv ? style.links.active : ''
+            )}
+          >
+            Pay per view
+          </Link>
+        </Col>
+      </Row>
+    </FormGroup>
+    <FormGroup
+      className={css(style.form.group)}
+      style={{
+        display:
+          (props.state.upload.file &&
+            attrAccept(props.state.upload.file, ATTR_ACCEPT_MEDIA)) ||
+          Object.keys(props.state.links).filter(a => props.state.links[a])
+            .length
+            ? `none`
+            : `block`
+      }}
+    >
+      <Row className={css(style.form.row)}>
+        <Col xs="12">
+          <h4 className={css(style.sliders.h4)}>Display time</h4>
+        </Col>
+      </Row>
+      <Row className={css(style.form.row)}>
+        <Col xs="10" className={css(style.sliders.wrapper)}>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={props.state.displayTime}
+            onChange={props.onDisplayTimeChange}
+            className={css(style.sliders.slider)}
+          />
+        </Col>
+        <Col xs="2" className={css(style.sliders.labelWrapper)}>
+          <span className={css(style.sliders.label)}>
+            {props.state.displayTime}h
+          </span>
+        </Col>
+      </Row>
+    </FormGroup>
+
+    <FormGroup className={css(style.form.group)}>
+      <Row className={css(style.form.row)}>
+        <Col xs="12">
+          <h4 className={css(style.sliders.h4)}>Validity period</h4>
+        </Col>
+      </Row>
+      <Row className={css(style.form.row)}>
+        <Col xs="5" className={css(style.sliders.wrapper)}>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={props.state.validity.hour}
+            onChange={props.onValidityHourChange}
+            className={css(style.sliders.slider)}
+          />
+        </Col>
+        <Col xs="1" className={css(style.sliders.labelWrapper)}>
+          <span className={css(style.sliders.label)}>
+            {props.state.validity.hour}h
+          </span>
+        </Col>
+        <Col xs="4" className={css(style.sliders.wrapper)}>
+          <input
+            type="range"
+            min="0"
+            max="4"
+            value={props.state.validity.minute / 15}
+            onChange={props.onValidityMinuteChange}
+            className={css(style.sliders.slider)}
+          />
+        </Col>
+        <Col xs="2" className={css(style.sliders.labelWrapper)}>
+          <span className={css(style.sliders.label)}>
+            {props.state.validity.minute}m
+          </span>
+        </Col>
+      </Row>
+    </FormGroup>
+
+    <FormGroup className={css(style.form.group, style.views.box)}>
+      <Row className={css(style.form.row)}>
+        <Col xs="12">
+          <h4 className={css(style.sliders.h4)}>Number of views</h4>
+        </Col>
+      </Row>
+      <Row className={css(style.form.row)}>
+        <Col xs="7" className={css(style.views.inputWrapper)}>
+          <Input
+            onChange={props.onViewsNumberChange}
+            type="text"
+            value={props.state.views.view ? props.state.views.number : ''}
+            placeholder="Number of views"
+            className={css(
+              style.views.input,
+              !props.state.views.view ? style.views.active : ''
+            )}
+            disabled={!props.state.views.view}
+          />
+        </Col>
+        <Col className="d-flex flex-column justify-content-center">
+          <input
+            type="checkbox"
+            id="views_checkbox"
+            checked={props.state.views.view}
+            onChange={props.onCheckboxClick}
+            className={css(style.views.checkbox)}
+          />
+          <label htmlFor="views_checkbox" className={css(style.views.label)}>
+            <span
+              className={css(
+                props.state.views.view
+                  ? style.views.spanChecked
+                  : style.views.span
+              )}
+            >
+              No limit views
+            </span>
+          </label>
+        </Col>
+      </Row>
+    </FormGroup>
+    <Button type="submit" className={css(style.form.submitButton)}>
+      Create link!
+    </Button>
+  </Form>
+)
+
+export default UploadForm
