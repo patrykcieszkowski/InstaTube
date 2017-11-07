59b043206411363558daf9cdfa2352ccebdbed8f
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-07 18:06:55 +0000

    - renames grid.js as main.js (styling) - sets margin to 0

--- src/components/Download/Main/style/index.js
+++ src/components/Download/Main/style/index.js
@@ -1,13 +1,13 @@
 import { StyleSheet } from 'aphrodite'
 
 import timer from './timer'
-import grid from './grid'
+import main from './main'
 import media from './media'
 import signup from './signup'
 
 export default {
   timer: StyleSheet.create(timer),
-  grid: StyleSheet.create(grid),
+  main: StyleSheet.create(main),
   media: StyleSheet.create(media),
   signup: StyleSheet.create(signup)
 }
