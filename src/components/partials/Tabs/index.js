797b81be8470e3d8efbf74e0b524df121b45dbb4
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-08 15:09:06 +0000

    exports Auth routes

--- src/components/partials/Tabs/index.js
+++ src/components/partials/Tabs/index.js
@@ -1,7 +1,13 @@
 import About from './About'
 import Help from './Help'
+import Auth from './Auth'
+import Remind from './Auth/Remind'
 
 export default {
   About,
-  Help
+  Help,
+  Auth: {
+    Auth,
+    Remind
+  }
 }
