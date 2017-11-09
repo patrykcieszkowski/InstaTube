b3c9dc3f2e6a3108b7bf3f7eba4fd5affcb084a6
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-09 14:52:04 +0000

    adds mobile support

--- src/components/partials/Alert/style/box.js
+++ src/components/partials/Alert/style/box.js
@@ -1,10 +1,13 @@
 export const boxStyle = {
   wrapper: {
-    width: `570px`,
+    width: `100%`,
     height: `200px`,
     background: `white`,
     boxShadow: `0px 8px 17px 0px rgba(0,0,0,0.15)`,
-    padding: `30px`
+    padding: `30px`,
+    '@media only screen and (min-width : 1200px)': {
+      width: `570px`,
+    }
   },
   h4: {
     color: `#3C2557`,
