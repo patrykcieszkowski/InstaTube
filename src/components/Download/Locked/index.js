7cd0f6713ac8c8bfa83a1e2aa3daa40c22b05e80
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-19 00:37:22 +0000

    - adds renderTimer function (displayed on non-premium content) - adds and applies onPPVSubmit function - adds and applies onPPIClick function - adds PayByInstagram button - hides content until media loads - adds redirection on 404 - adds redirection to /locked on locked content - replaces PieTimer component injection with PieTimer function - adds actual API result support - adds match prop to all Sidebar content components - adds fetch action to the Media store - adds pay action to the Media store

--- src/components/Download/Locked/index.js
+++ src/components/Download/Locked/index.js
@@ -1,11 +1,56 @@
 import React from 'react'
+/* eslint-disable no-unused-vars */
+import { inject, observer } from 'mobx-react'
+/* eslint-enable no-unused-vars */
 
 import LockedComponent from '../partials/Locked'
 
+@inject('media', 'user', 'auth')
+@observer
 export class Locked extends React.Component {
+  constructor (props) {
+    super(props)
+    const state = {}
+    state.ppv = {}
+    this.state = state
+  }
+
+  onPPVSubmit (e) {
+    e.preventDefault()
+
+    if (!this.props.auth.auth.local) {
+      return (window.location.href = '/auth')
+    }
+
+    const data = {
+      email: this.props.user.dashboard.email,
+      hash: this.props.match.params.mediaId,
+      type: this.props.media.media.amount
+    }
+
+    this.props.media.pay(data)
+  }
+
+  onPPIClick (e) {
+    e.preventDefault()
+
+    if (!this.props.auth.auth.local) {
+      return (window.location.href = '/auth')
+    }
+
+    const { instagram } = this.props.media.media
+    window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${instagram.apikey}&redirect_uri=${instagram.url}&response_type=code&scope=public_content+follower_list`
+  }
+
   render () {
-    return <LockedComponent />
+    return (
+      <LockedComponent
+        media={this.props.media.media || {}}
+        onPPVSubmit={this.onPPVSubmit.bind(this)}
+        onPPIClick={this.onPPIClick.bind(this)}
+      />
+    )
   }
 }
 
-export default LockedComponent
+export default Locked
