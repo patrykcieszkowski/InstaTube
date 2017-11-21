6c80819af5ffb06e0d86520a8307a829629241ce
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-21 17:24:37 +0000

    - rebuilds Social store data structure - adds FormAlert to the Social tab - adds delete action to Social store

--- src/components/Dashboard/Social/partials/SocialHistory/partials/listItem.js
+++ src/components/Dashboard/Social/partials/SocialHistory/partials/listItem.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { css } from 'aphrodite'
 import { Row, Col } from 'reactstrap'
 
+import Column from '../../../../../partials/Col'
+
 import style from '../../../style'
 
 export const PayoutHistoryItem = props => {
-  const date = new Date(props.item.date)  
+  const date = new Date(props.item.date)
   return (
-    <Row className={css(style.history.box)}>
+    <Row
+      className={css(
+        style.history.box,
+        props.item.hide ? style.history.boxHidden : ''
+      )}
+    >
       <Col xs='12' className={css(style.history.innerWrapper)}>
         <Row>
           <Col xs='6'>
@@ -15,14 +22,48 @@ export const PayoutHistoryItem = props => {
               @{props.item.name}
             </span>
           </Col>
-          <Col xs='6'>
-            <span className={css(style.history.valueSpan, style.history.dateSpan)}>
-              { `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}` }
+          <Col xs='4'>
+            <span
+              className={css(style.history.valueSpan, style.history.dateSpan)}
+            >
+              {`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`}
             </span>
+          </Col>
+          <Col xs='2'>
+            <Row>
+              <ActionsColumn onDeleteItemClick={props.onDeleteItemClick} />
+            </Row>
           </Col>
         </Row>
       </Col>
     </Row>
+  )
+}
+
+const ActionsColumn = props => (
+  <Column xs='12' xl='' xxxxl='auto'>
+    <RenderActions {...props} />
+  </Column>
+)
+
+const RenderActions = props => {
+  return (
+    <span
+      className={`${css(
+        style.history.valueSpan
+      )} d-flex justify-content-xl-center align-items-center`}
+    >
+      <a href='#delete' onClick={props.onDeleteItemClick}>
+        <i
+          className={`la la-trash-o ${css(
+            style.history.actionIcon,
+            style.history.actionIconTrash
+          )}`}
+          aria-hidden='true'
+          title='expand'
+        />
+      </a>
+    </span>
   )
 }
 
