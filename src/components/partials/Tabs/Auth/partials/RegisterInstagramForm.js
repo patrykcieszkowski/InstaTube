11e10f0c4ccc5805ab59263ad9a6886199079b23
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-23 12:40:51 +0000

    - adds rules checkbox to the instagram register page

--- src/components/partials/Tabs/Auth/partials/RegisterInstagramForm.js
+++ src/components/partials/Tabs/Auth/partials/RegisterInstagramForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { css } from 'aphrodite'
 import { Form, FormGroup, Input, Button, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 import style from '../style'
 
@@ -8,7 +9,7 @@ export const RegisterInstagramForm = props => (
   <Form className={css(style.form.form)} onSubmit={props.onSubmit}>
     <FormGroup className={css(style.form.group)}>
       <Input
-        placeholder='E-mail address'
+        placeholder="E-mail address"
         className={css(style.inputs.input)}
         name={`email`}
         onChange={props.onTextChange}
@@ -17,8 +18,8 @@ export const RegisterInstagramForm = props => (
 
     <FormGroup className={css(style.form.group)}>
       <Input
-        placeholder='Password'
-        type='password'
+        placeholder="Password"
+        type="password"
         className={css(style.inputs.input)}
         name={`password`}
         onChange={props.onTextChange}
@@ -27,19 +28,45 @@ export const RegisterInstagramForm = props => (
 
     <FormGroup className={css(style.form.group)}>
       <Input
-        placeholder='Confirm password'
-        type='password'
+        placeholder="Confirm password"
+        type="password"
         className={css(style.inputs.input)}
         name={`confirm`}
         onChange={props.onTextChange}
       />
     </FormGroup>
 
+    <FormGroup>
+      <input
+        type="checkbox"
+        id="register_rules_checkbox"
+        checked={props.rulesValue}
+        onChange={props.onRulesClick}
+        className={css(style.checkbox.checkbox)}
+      />
+      <label
+        htmlFor="register_rules_checkbox"
+        className={css(style.checkbox.label)}
+      >
+        <span
+          className={css(
+            props.rulesValue ? style.checkbox.spanChecked : style.checkbox.span
+          )}
+        >
+          I accept the{' '}
+          <Link to="/rules" className={css(style.checkbox.link)}>
+            rules
+          </Link>{' '}
+          on the site
+        </span>
+      </label>
+    </FormGroup>
+
     <FormGroup className={css(style.form.group)}>
-      <Col xs='12' xl='6' style={{ padding: 0 }}>
+      <Col xs="12" xl="6" style={{ padding: 0 }}>
         <Button
           className={css(style.buttons.button, style.buttons.signup)}
-          type='submit'
+          type="submit"
         >
           Sign me up!
         </Button>
