0dfde5bcf9c56a8e989f4bcf52ef9fd5d6508ba5
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-24 14:08:24 +0000

    - adds Social link on profile dropdown (mobile view) - reorders links on profile dropdown (mobile view) - adds logo

--- src/components/partials/Navbar/style/index.js
+++ src/components/partials/Navbar/style/index.js
@@ -3,9 +3,11 @@ import { StyleSheet } from 'aphrodite'
 import navbar from './navbar'
 import button from './button'
 import collapse from './collapse'
+import logo from './logo'
 
 export default {
   navbar: StyleSheet.create(navbar),
   button: StyleSheet.create(button),
-  collapse: StyleSheet.create(collapse)
+  collapse: StyleSheet.create(collapse),
+  logo: StyleSheet.create(logo)
 }
