039f7fee98cb9ba55c22800b3a2fd8f72a41e75b
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-24 17:49:09 +0000

    - removes FB injection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* global FB */
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Provider } from 'mobx-react'
@@ -23,26 +22,6 @@ class App extends React.Component {
         stores.user.fetchDashboard()
       }, 1000 * 60 * 5)
     }
-
-    // inject facebook sdk script
-    window.fbAsyncInit = () => {
-      FB.init({
-        appId: process.env.REACT_APP_FB_ID,
-        xfbml: true,
-        version: 'v2.6'
-      })
-    }
-    ;(function (d, s, id) {
-      var js = null
-      var fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) {
-        return
-      }
-      js = d.createElement(s)
-      js.id = id
-      js.src = '//connect.facebook.net/en_US/sdk.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    })(document, 'script', 'facebook-jssdk')
   }
 
   render () {
