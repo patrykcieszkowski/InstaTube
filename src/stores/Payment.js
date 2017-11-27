2ea5581d373ffe0857c22bb0fb0c35634d1390f6
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-11-27 15:35:24 +0000

    - creates Payment store - injects payment store to the Payment component - runs PayByCard action on payment submit - adds priority for response errors

---
+++ src/stores/Payment.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import { observable, action } from 'mobx'
+import axios from 'axios'
+
+class Payment {
+  @observable response = null
+
+  @action
+  payByCard(data) {
+    const formData = new FormData()
+    Object.keys(data).forEach(key => formData.append(`pay[${key}]`, data[key]))
+
+    const API_URL = process.env.REACT_APP_API_URL
+    axios
+      .post(`${API_URL}/payment/paybycard`)
+      .then(res => {
+        this.response = res.data
+        if (res.data.success) {
+          window.location.reload()
+        }
+      })
+      .catch(err => {
+        this.response = err.response.data
+      })
+  }
+}
+
+export default new Payment()
